30b3c547ee806c3716217e7de51e22799d6e05c6
Patrick Göler von Ravensburg <patrickvongoeler@gmail.com>
2021-11-01 12:13:27 +0100

    prisma setup

---
+++ prisma/migrations/20211101111151_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "isDone" BOOLEAN NOT NULL DEFAULT false
+);
